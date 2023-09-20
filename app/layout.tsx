30ec9f8f9a1f6cd5f4cd1aeb8a862ac3d83bc049
kitions <nsplknsplk@gmail.com>
2023-09-20 14:35:27 +0800

    feat: add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import { Analytics } from '@vercel/analytics/react';
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -44,6 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             {children}
             <TailwindIndicator />
             <Toaster />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
